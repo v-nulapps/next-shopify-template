36b304ebd418a4d7ced881839c8cf3073f070b94
v-nulapps <vladan@nulapps.nl>
2023-03-28 15:50:55 +0200

    updated next

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import "../styles/globals.css";
 import { Cursor } from "../components/Cursor";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main>
+    <main className={nunito.className}>
       <Cursor />
       <Header />
       <Component {...pageProps} />
